1a961c36e96b3b9be3dc16c7a984905603c2c249
prithvi2k2 <prithvi2k2@gmail.com>
2023-11-13 20:07:58 +0530

    EXPERIMENTAL - Auto Animations Added
    Powered by FormKit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [parent, enableAnimations] = useAutoAnimate();
+  return (
+    <div ref={parent}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;

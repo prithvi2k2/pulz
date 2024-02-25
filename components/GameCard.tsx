0715c8780a419986e0cb7e989c97ba759458da59
prithvi2k2 <prithvi2k2@gmail.com>
2024-02-25 17:59:23 +0530

    Update GameCard.tsx
    fixed width cards... independent of content inside

--- components/GameCard.tsx
+++ components/GameCard.tsx
@@ -7,7 +7,7 @@ import Router from "next/router";
 // This component is based on HyperUI/Marketing/Card
 export default function GameCard({ ...props }) {
   return (
-    <div className="m-3 group relative block h-64 sm:h-80 lg:h-96">
+    <div className="m-3 group relative block h-64 sm:h-80 lg:h-96 w-72">
       <span className="absolute inset-0 border-2 border-dashed border-black"></span>
 
       <div className="relative flex h-full transform items-end border-2 border-black bg-white transition-transform group-hover:-translate-x-2 group-hover:-translate-y-2">
